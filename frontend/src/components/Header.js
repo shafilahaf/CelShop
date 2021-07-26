fc70d664b06cbfa0ff185a7d638cd6a157b90c55
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-07-27 02:19:35 +0700

    First Commit
    Next homescreen-product-listing

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+
+const Header = () => {
+    return <header>
+    <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
+    <Container>
+      <Navbar.Brand href="/">Celshop</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ms-auto">
+          <Nav.Link href="/cart">
+            <i className='fas fa-cart-arrow-down'></i>Cart
+            </Nav.Link>
+          <Nav.Link href="/login">
+            <i className="fas fa-user-alt"></i>Sign In
+            </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  </header>
+}
+
+export default Header
