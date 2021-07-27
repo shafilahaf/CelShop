5958fd4a2ff8fcce6232e1f2b99156475bf5fdc6
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-07-27 12:13:54 +0700

    Screen
    Please fix the button cart

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
     return <header>
     <Navbar bg="primary" variant='dark' expand="lg" collapseOnSelect>
     <Container>
-      <Navbar.Brand href="/">Celshop</Navbar.Brand>
+      <LinkContainer to='/'>
+      <Navbar.Brand>Celshop</Navbar.Brand>
+      </LinkContainer>
+      
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ms-auto">
-          <Nav.Link href="/cart">
+          <LinkContainer to='/cart'>
+          <Nav.Link>
             <i className='fas fa-cart-arrow-down'></i>Cart
             </Nav.Link>
-          <Nav.Link href="/login">
+          </LinkContainer>
+          <LinkContainer to='/login'>
+          <Nav.Link>
             <i className="fas fa-user-alt"></i>Sign In
             </Nav.Link>
+          </LinkContainer>
         </Nav>
       </Navbar.Collapse>
     </Container>
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+    {
+        _id: "1",
+        name: "Divoom Ditoo Pixel Display Bluetooth Speaker",
+        image: '/image/Divoom Ditoo Pixel Display Bluetooth Speaker.jpg',
+        description: 
+        "Meet Divoom Ditoo- Our most popular retro Bluetooth speaker with a 3.55 inch led screen. 10W DSP Bluetooth Speaker- The Divoom Ditoo portable Bluetooth speaker with 10w high-performance drivers, makes it pump out more volume; comes with the unique DSP bass boost mode DIY Pixel Art Kit- We also prepare numerous pixel art creation tools for you in Divoom APP",
+        brand: "Divoom International",
+        category: "Electornics",
+        price: 80.99,
+        countInStock: 5,
+        rating: 4.5,
+        numReviews: 4,
+    },
+    {
+        _id: "2",
+        name: "Divoom Ditoo Retro Pixel Art Game Bluetooth Speaker",
+        image: '/image/Divoom Ditoo Retro.jpg',
+        description: 
+        "It is also good that you can raise and lower the volume using the keyboard has a pretty retro feeling. The sound is also better than a smartphone, and I think that it can be used enough as a speaker. And more than that, this speaker is too feature-rich.",
+        brand: "Divoom International",
+        category: "Electornics",
+        price: 75.99,
+        countInStock: 2,
+        rating: 4.0,
+        numReviews: 3,
+    },
+    {
+        _id: "3",
+        name: "Mixed Color Round Key Cap Wireless Keyboard And Mouse",
+        image: '/image/Mixed Color Round Key Cap Wireless Keyboard And Mouse.jpg',
+        description: 
+        "Model: MCK15678934 Interface: USB Connection: Wireless Material: plastic/electronic component Color :pink mixed color/ blue mixed color/ green mixed color/ lipstick mixed color System compatible: XP/ 7/ 8/ 10",
+        brand: "Evancas",
+        category: "Electornics",
+        price: 59.32,
+        countInStock: 10,
+        rating: 4.9,
+        numReviews: 5,
+    },
+    {
+        _id: "4",
+        name: "Fujifilm Instax Mini 11 CameraSelfie",
+        image: '/image/Fujifilm Instax Mini 11 CameraSelfie.jpg',
+        description: 
+        "Fujifilm Instax Mini 11 CameraSelfie ModePull & PoutSelfies (and close-up shots) couldn’t be easier. You simply need to pull the front edge of the lens, and as if by magic, a slick built-in selfie lens, the same color as the camera body, will appear. Then hold the instax mini 11 at arms length, practice your pout in the selfie mirror and snap away.",
+        brand: "Target",
+        category: "Electornics",
+        price: 32.99,
+        countInStock: 7,
+        rating: 3.2,
+        numReviews: 2,
+    },
+    {
+        _id: "5",
+        name: "Waterproof Action Camera - Silver",
+        image: '/image/Waterproof Action Camera - Silver.jpg',
+        description: 
+        "This high-definition waterproof action camera can be used in many applications. From extreme sports, outdoor activities, bicycles, car DVRs, diving to web cameras, and many other preferred activities. ",
+        brand: "Bendy",
+        category: "Electornics",
+        price: 35.55,
+        countInStock: 0,
+        rating: 4,
+        numReviews: 1,
+    },
+    {
+        _id: "6",
+        name: "HP Sprocket Portable Bluetooth Photo Printer White",
+        image: '/image/HP Sprocket Portable Bluetooth Photo Printer White.jpg',
+        description: 
+        "We all have those friends who are addicted to Instagram. Whether they're documenting everything on Instagram Stories or constantly creating the perfectly. ",
+        brand: "Pop Sugar",
+        category: "Electornics",
+        price: 25.59,
+        countInStock: 7,
+        rating: 3.7,
+        numReviews: 3,
+    },
+]
+
+export default products
