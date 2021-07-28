e1b6d2bbc13d5a6cf3809f3923e26711adafe58c
SHAFILAH AF <61927381+shafilahaf@users.noreply.github.com>
2021-07-28 15:45:21 +0700

    Fixed

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -9,7 +9,7 @@ const Product = ({product}) => {
     return (
         <Card className='my-3 p-3 rounded'>
             <Link to={`/product/${product._id}`}>
-                <Card.Img src= {product.image} variant="top" />
+                <Card.Img src= {product.imageUrl} variant="top" />
             </Link>
 
             <Card.Body>
--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -20,7 +20,7 @@ const Homescreen = () => {
         <>
           <h1>Latest Products</h1>
           <Row>
-              {products.map(product =>(
+              {products.map((product) =>(
                   <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
                   <Product product={product} />
                   </Col>
