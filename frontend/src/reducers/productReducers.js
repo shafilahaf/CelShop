de35ff960accb249172e3f1563fa85b345062fe9
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-07-30 19:16:24 +0700

    Shopping Cart
    next Cart Reducer and Add to Cart Action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,5 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstants.js'
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL} from '../constants/productConstants.js'
 
 
 
@@ -10,6 +11,20 @@ export const  productListReducers = (state = { products: []}, action) =>{
             return { loading: false, products:action.payload}
         case PRODUCT_LIST_FAIL:
             return {loading: false, products:action.payload}
+
+        default:
+            return state
+    }
+}
+
+export const  productDetailsReducers = (state = { product: {reviews: []}}, action) =>{
+    switch(action.type){
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true, ...state}
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product:action.payload}
+        case PRODUCT_DETAILS_FAIL:
+            return {loading: false, product:action.payload}
 
         default:
             return state
