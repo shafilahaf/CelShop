465ec5b4b2b43cf1720f5b85cee23a10f666b226
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-08-03 12:39:57 +0700

    Auth endpoint
    cp7 sec 2

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,23 +1,10 @@
 import express from'express'
 const router = express.Router()
-import Product from '../models/productModel.js'
-import asyncHandler from 'express-async-handler'
+import { getProductById, getProducts } from '../controllers/productController.js'
 
 
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-
-    if(product){
-        res.json(product)
-    } else{
-        res.status(404).json({message: "Product not found"})
-    }
-    
-}))
+router.route('/:id').get(getProductById)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
-import products from './data/products.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -10,12 +10,14 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.send('API is running...<3')
 })
 
 app.use('/api/products', productRoutes)
-
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
