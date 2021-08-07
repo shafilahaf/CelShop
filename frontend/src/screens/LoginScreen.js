0793f20b81d185e33db5d10aba8f7da7147ffb03
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-08-07 09:41:56 +0700

    Update LoginScreen.js

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -35,7 +35,7 @@ const LoginScreen = ({location, history}) => {
     return (
     <FormContainer>
         <h1>Sign In</h1>
-        {error && <Message variant='danger'>{error}</Message>}
+        {error && <Message variant='danger'>Invalid email or password</Message>}
         {loading && <Loader/>}
         <Form onSubmit={submitHandler}>
             <Form.Group controlId='email'>
